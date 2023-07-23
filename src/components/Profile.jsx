ad1961e8d1bde22379d7a9083e5fa934c029a37b
sohseyedi <sohseyedi@gmail.com>
2023-07-23 18:59:42 +0330

    components router

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,20 @@
-import Container from './../container/Container';
+import Container from "./../container/Container";
+import * as RiIcon from "react-icons/ri";
 
 const Profile = () => {
   return (
-    <Container>Profile</Container>
-  )
-}
+    <Container>
+      <section className="flex-1 h-screen overflow-y-auto">
+        <div className="w-[95%] mx-auto pt-6">
+          <h3 className="text-xl pb-3 border-b border-gray-300 lg:text-2xl font-semibold text-[#252525]">
+            پروفایل
+          </h3>
 
-export default Profile
+          <div className="py-5"></div>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Profile;
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Container from './../container/Container';
+import * as RiIcon from "react-icons/ri";
+
+import Container from "./../container/Container";
 
 const Settings = () => {
   return (
-    <Container>Settings</Container>
-  )
-}
+    <Container>
+      <section className="flex-1 h-screen overflow-y-auto">
+        <div className="w-[95%] mx-auto  pt-6">
+          <h3 className="text-xl lg:text-2xl pb-3 border-b border-gray-300 font-semibold text-[#252525]">
+            تنظیمات
+          </h3>
 
-export default Settings
+          <div className="py-5"></div>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Settings;
